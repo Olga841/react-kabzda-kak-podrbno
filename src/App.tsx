cb391647e61aec172ecfee9bc30e6587f25e591e
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-05-13 06:11:30 +0300

    Add function/component 'Accordion'

--- src/App.tsx
+++ src/App.tsx
@@ -21,4 +21,17 @@ function Rating() {
   )
 }
 
+function Accordion() {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+
 export default App;
