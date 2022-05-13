39fb123f05c6c335363b00f6dee7691b87e72e77
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-05-13 06:47:31 +0300

    Create function/component AcoordionTitle, AccordionBody add to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
+    console.log('App rendering...')
     return (
         <div>
             <AppTitle/>
@@ -12,6 +13,7 @@ function App() {
 }
 
 function Rating() {
+    console.log('Rating rendering...')
   return (
       <div>
         <Star/>
@@ -24,14 +26,11 @@ function Rating() {
 }
 
 function Accordion() {
+    console.log('Accordion rendering...')
     return (
         <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle/>
+            <AccordionBody/>
         </div>
     )
 }
@@ -41,7 +40,24 @@ function Star() {
 }
 
 function AppTitle() {
+    console.log('AppTitle rendering...')
     return <>This is APP component</>
+}
+
+function AccordionTitle() {
+    console.log('AccordionTitle rendering...')
+    return <h3>Menu</h3>
+}
+
+function AccordionBody() {
+    console.log('AccordionBody rendering...')
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
 }
 
 export default App;
