f38076d3a6065f01b4b53a3cfaed5e4fbba33f26
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-05-15 21:11:48 +0300

    Add renders of Rating in App

--- src/Rating.tsx
+++ src/Rating.tsx
@@ -78,7 +78,7 @@ function Star(props: StarPropsType) {
     if (props.selected) {
         return <span><b>star </b></span>
     } else {
-        return <span>star</span>
+        return <span>star </span>
     }
 
 }
