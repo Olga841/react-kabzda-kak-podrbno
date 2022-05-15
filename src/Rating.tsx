f2127042567b40429e4c1607a94fbeedeb64e177
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-05-15 20:47:55 +0300

    Add props to Star

--- src/Rating.tsx
+++ src/Rating.tsx
@@ -4,15 +4,23 @@ export function Rating() {
     console.log('Rating rendering...')
     return (
         <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
         </div>
     )
 }
 
-function Star() {
-    return <div>star</div>
+type StarPropsType = {
+    selected: boolean
+}
+function Star(props: StarPropsType) {
+    if (props.selected) {
+        return <span><b>star</b></span>
+    } else {
+        return <span>star</span>
+    }
+
 }
