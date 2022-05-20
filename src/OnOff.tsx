6d0242c3bb258197d4de8c51afdd3ff5a87eebba
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-05-20 06:51:29 +0300

    Refactor OnOff component

---
+++ src/OnOff.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function OnOff() {
+    return (
+        <>
+            <div>On</div>
+            <div>Off</div>
+            <div></div>
+        </>
+    )
+}
