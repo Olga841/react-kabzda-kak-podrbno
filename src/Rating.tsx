db22ca441b67d192786ebf29e96e9f484be69319
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-06-18 17:15:23 +0300

    Refactor Rating without hardcode

--- src/Rating.tsx
+++ src/Rating.tsx
@@ -6,20 +6,21 @@ export function Rating() {
     console.log('Rating rendering...')
     return (
         <div>
-            <Star selected={value > 0} setValue={setValue}/>
-            <Star selected={value > 0} setValue={setValue}/>
-            <Star selected={value > 0} setValue={setValue}/>
-            <Star selected={value > 0} setValue={setValue}/>
-            <Star selected={value > 0} setValue={setValue}/>
+            <Star selected={value > 0} setValue={setValue} value={1}/>
+            <Star selected={value > 1} setValue={setValue} value={2}/>
+            <Star selected={value > 2} setValue={setValue} value={3}/>
+            <Star selected={value > 3} setValue={setValue} value={4}/>
+            <Star selected={value > 4} setValue={setValue} value={5}/>
         </div>
     )
 }
 
 type StarPropsType = {
+    value: 1 | 2 | 3 | 4 | 5
     setValue: (value: 1 | 2 | 3 | 4 |5) => void
     selected: boolean
 }
 
 function Star(props: StarPropsType) {
-    return <span onClick={() => {props.setValue(5)}}>{props.selected ? <b> star </b> : ' star '}</span>
+    return <span onClick={() => {props.setValue(props.value)}}>{props.selected ? <b> star </b> : ' star '}</span>
 }
